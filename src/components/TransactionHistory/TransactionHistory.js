c600358666a9a5e6a55a68a30308680b11ade9ac
Jasper935 <102736176+Jasper935@users.noreply.github.com>
2022-07-19 21:25:55 +0300

    upd

--- src/components/TransactionHistory/TransactionHistory.js
+++ src/components/TransactionHistory/TransactionHistory.js
@@ -28,7 +28,7 @@ export const TransactionHistory=({transactions})=>{
 TransactionHistory.propTypes = {
   transactions: PropTypes.arrayOf(
     PropTypes.shape({
-      id: PropTypes.string,
+      
       type: PropTypes.string.isRequired,
       amount: PropTypes.string.isRequired,
       id: PropTypes.string.isRequired,
